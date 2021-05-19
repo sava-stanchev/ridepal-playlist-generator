52c7b56fa3d9a77e3e4593003232099791c7adf4
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-19 22:34:42 +0300

    add some routes to the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Seed from './components/Seed'
 import NavBar from "./components/Navbar";
+import Login from './components/Login';
+import Register from './components/Register';
+import {useState} from 'react';
+import AuthContext, {getUser} from './providers/auth-context';
 import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import StartPage from './components/StartPage';
 
-function App() {
+const App = () => {
+  const [authValue, setAuthValue] = useState({
+    user: getUser(),
+    isLoggedIn: Boolean(getUser()),
+  })
+
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <Seed />
+        <AuthContext.Provider value = {{...authValue, setAuthState: setAuthValue}}>
+          <NavBar />
+          <Switch>
+            <Redirect path="/" exact to="/home" />
+            <Route path="/home" exact component={StartPage}/>
+            <Route path="/login" component={Login}/>       
+            <Route path="/register" component={Register}/>
+          </Switch>
+        </AuthContext.Provider>
       </BrowserRouter>
     </div>
   );
