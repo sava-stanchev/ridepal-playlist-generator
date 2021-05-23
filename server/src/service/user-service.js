d64b2af8136c416b17a1d536784af64d900b6234
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-23 10:56:15 +0300

    fix createUser

--- server/src/service/user-service.js
+++ server/src/service/user-service.js
@@ -2,18 +2,12 @@ import usersData from '../data/users.js';
 import bcrypt from 'bcrypt';
 
 const createUser = async (userData) => {
-  const usernameExist = await usersData.getUserByName(userData.username);
-  if (usernameExist[0]) {
+  const usernameExists = await usersData.getUserByName(userData.username);
+  if (usernameExists[0]) {
     return null;
   }
 
   userData.password = await bcrypt.hash(userData.password, 10);
-  if (typeof userData.user_age === 'undefined') {
-    userData.user_age = 'DEFAULT';
-  }
-  if (typeof userData.gender === 'undefined') {
-    userData.gender = 'DEFAULT';
-  }
   const newUser = await usersData.createUser(userData);
   return newUser;
 };
