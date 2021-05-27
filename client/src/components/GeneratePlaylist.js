b7838e5bffb765f4eea17d1c3a51d064ad952df8
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-27 09:37:19 +0300

    slider logic + basic html css

--- client/src/components/GeneratePlaylist.js
+++ client/src/components/GeneratePlaylist.js
@@ -1,7 +1,27 @@
+import {useState} from "react";
+
 const GeneratePlaylist = () => {
+  const [sliderOne, setSliderOne] = useState(50);
 
   return(
-      <></>
+    <section className="join-login-main-section">
+      <h1 className="join-login-text">
+        Choose
+        <span className="accent-text"> genres!</span>
+      </h1>
+      <form className="join-login-form">
+        <div className="slider-main">
+          <input type="range" min={0} max={100} value={sliderOne} id="slider" onChange={(e) => setSliderOne(e.target.value)}/>
+          <div id="selector">
+            <div className="SelectBtn"></div>
+            <div id="SelectValue">Jazz: {sliderOne}%</div>
+          </div>
+        </div>
+        <div className="input-group">
+          <button type="submit" className="btn">Generate</button>
+        </div>
+      </form>
+    </section>
   )
 };
 
