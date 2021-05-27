2914b7da2e7c6d604ffcaacb52b62550a678ab4a
Sava Stanchev <s.stanchev94@gmail.com>
2021-05-27 10:45:57 +0300

    add more sliders and improve html css

--- client/src/components/GeneratePlaylist.js
+++ client/src/components/GeneratePlaylist.js
@@ -2,6 +2,10 @@ import {useState} from "react";
 
 const GeneratePlaylist = () => {
   const [sliderOne, setSliderOne] = useState(50);
+  const [sliderTwo, setSliderTwo] = useState(50);
+  const [sliderThree, setSliderThree] = useState(50);
+  const [sliderFour, setSliderFour] = useState(50);
+  const [sliderFive, setSliderFive] = useState(50);
 
   return(
     <section className="join-login-main-section">
@@ -11,11 +15,34 @@ const GeneratePlaylist = () => {
       </h1>
       <form className="join-login-form">
         <div className="slider-main">
-          <input type="range" min={0} max={100} value={sliderOne} id="slider" onChange={(e) => setSliderOne(e.target.value)}/>
           <div id="selector">
-            <div className="SelectBtn"></div>
             <div id="SelectValue">Jazz: {sliderOne}%</div>
           </div>
+          <input type="range" min={0} max={100} value={sliderOne} id="slider" onChange={(e) => setSliderOne(e.target.value)}/>
+        </div>
+        <div className="slider-main">
+          <div id="selector">
+            <div id="SelectValue">Rock: {sliderTwo}%</div>
+          </div>
+          <input type="range" min={0} max={100} value={sliderTwo} id="slider" onChange={(e) => setSliderTwo(e.target.value)}/>
+        </div>
+        <div className="slider-main">
+          <div id="selector">
+            <div id="SelectValue">Blues: {sliderThree}%</div>
+          </div>
+          <input type="range" min={0} max={100} value={sliderThree} id="slider" onChange={(e) => setSliderThree(e.target.value)}/>
+        </div>
+        <div className="slider-main">
+          <div id="selector">
+            <div id="SelectValue">Disco: {sliderFour}%</div>
+          </div>
+          <input type="range" min={0} max={100} value={sliderFour} id="slider" onChange={(e) => setSliderFour(e.target.value)}/>
+        </div>
+        <div className="slider-main">
+          <div id="selector">
+            <div id="SelectValue">Pop: {sliderFive}%</div>
+          </div>
+          <input type="range" min={0} max={100} value={sliderFive} id="slider" onChange={(e) => setSliderFive(e.target.value)}/>
         </div>
         <div className="input-group">
           <button type="submit" className="btn">Generate</button>
