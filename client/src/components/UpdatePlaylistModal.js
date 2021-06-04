14980c3e2f7a179b9ed2fc6d7118f5b375dbe5db
Sava Stanchev <s.stanchev94@gmail.com>
2021-06-04 09:54:30 +0300

    update playlist props

--- client/src/components/UpdatePlaylistModal.js
+++ client/src/components/UpdatePlaylistModal.js
@@ -1,20 +1,52 @@
-import ReactDom from 'react-dom'
+import { useState } from 'react';
+import ReactDom from 'react-dom';
+import {HOST} from '../common/constants.js';
 
-export default function Modal({playlist, updatePlaylistProps, open, onClose}) {
+export default function Modal({playlist, open, onClose}) {
+  const [thePlaylist, setThePlaylist] = useState(null);
+  console.log(playlist);
   if (!playlist) return null;
   if (!open) return null;
-  console.log(playlist);
+
+  console.log('first playlist');
+  console.log(thePlaylist);
+
+  const updatePlaylistProps = (prop, value) => {
+    console.log(value);
+    setThePlaylist({
+      ...thePlaylist,
+      [prop]: value,
+    });
+    console.log(thePlaylist);
+  };
+  
+  const updatePlaylist = () => {
+    fetch(`${HOST}/playlists/${playlist.playlists_id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(thePlaylist),
+    })
+    .then((res) => res.json())
+  };
+
+  const closeFunction = () => {
+    updatePlaylist();
+    onClose();
+  }
+
   return ReactDom.createPortal(
     <>
       <div className="overlay-styles" />
       <div className="modal-styles">
         <div className="input-group">
           <label>New playlist name:</label>
-          <input type="text" name="new-playlist-name" value={playlist.playlist_name}
+          <input type="text" name="playlist_name" value={thePlaylist ? thePlaylist.playlist_name : playlist.playlist_name}
           onChange={e => updatePlaylistProps('playlist_name', e.target.value)} />
         </div>
         <div className="input-group">
-          <button type="submit" className="btn" onClick={onClose}>Update</button>
+          <button type="submit" className="btn" onClick={closeFunction}>Update</button>
         </div>
       </div>
     </>,
