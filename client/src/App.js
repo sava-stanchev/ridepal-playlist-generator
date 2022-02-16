652ce0dbb9faafa5ece279ef4fb9b4d8d2e578a1
Sava Stanchev <s.stanchev94@gmail.com>
2022-02-16 21:55:19 +0200

    started converting css to sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,58 @@
-import './App.css';
+import "./App.scss";
 import NavBar from "./components/Navbar";
-import Login from './components/Login';
-import Register from './components/Register';
-import Users from './components/Users';
-import {useState} from 'react';
-import AuthContext, {getUser} from './providers/auth-context';
-import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
-import StartPage from './components/StartPage';
-import ViewPlaylist from './components/ViewPlaylist';
-import GenerateRoute from './components/GenerateRoute';
-import GeneratePlaylist from './components/GeneratePlaylist';
-import PageNotFound from './components/PageNotFound';
-import ServerError from './components/ServerError';
-import GuardedRoute from './providers/GuardedRoute';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Users from "./components/Users";
+import { useState } from "react";
+import AuthContext, { getUser } from "./providers/auth-context";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import StartPage from "./components/StartPage";
+import ViewPlaylist from "./components/ViewPlaylist";
+import GenerateRoute from "./components/GenerateRoute";
+import GeneratePlaylist from "./components/GeneratePlaylist";
+import PageNotFound from "./components/PageNotFound";
+import ServerError from "./components/ServerError";
+import GuardedRoute from "./providers/GuardedRoute";
 
 const App = () => {
   const [authValue, setAuthValue] = useState({
     user: getUser(),
     isLoggedIn: Boolean(getUser()),
-  })
+  });
 
   const [points, setPoints] = useState({
     duration: 0,
-    from: '',
-    to: '',
+    from: "",
+    to: "",
   });
 
   return (
     <div className="App">
       <BrowserRouter>
-        <AuthContext.Provider value = {{...authValue, setAuthState: setAuthValue}}>
+        <AuthContext.Provider
+          value={{ ...authValue, setAuthState: setAuthValue }}
+        >
           <NavBar />
           <Switch>
             <Redirect path="/" exact to="/home" />
             <Route exact path="/home" component={StartPage} />
-            <Route exact path="/login" component={Login} />       
+            <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <GuardedRoute exact admin path="/users" component={Users} />
-            <GuardedRoute exact path="/generate-route" component={(props) => <GenerateRoute {...props} setPoints={setPoints}/>} />
-            <GuardedRoute exact path="/generate-playlist" component={(props) => <GeneratePlaylist {...props} points={points}/>} />
+            <GuardedRoute
+              exact
+              path="/generate-route"
+              component={(props) => (
+                <GenerateRoute {...props} setPoints={setPoints} />
+              )}
+            />
+            <GuardedRoute
+              exact
+              path="/generate-playlist"
+              component={(props) => (
+                <GeneratePlaylist {...props} points={points} />
+              )}
+            />
             <Route exact path="/playlists/:id" component={ViewPlaylist} />
             <Route path="/500" component={ServerError} />
             <Route path="*" component={PageNotFound} />
@@ -47,6 +61,6 @@ const App = () => {
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
