864acb58a1107b0152b22dc9da904310950488b0
Sava Stanchev <s.stanchev94@gmail.com>
2024-11-30 08:51:50 +0200

    slider label for input id accessibility

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,10 +1,13 @@
 export const Slider = ({ title, value, onChange }) => {
   return (
     <div className="genre-slider">
-      <span className="genre-slider__title">{title}</span>
+      <label htmlFor={title} className="genre-slider__title">
+        {title}
+      </label>
       <div className="genre-slider__input-container">
         <input
           type="range"
+          id={title}
           className="genre-slider__input"
           min={0}
           max={100}
