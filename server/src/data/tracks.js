fdbc69121ff1643147c2c9508118118c905bc6a7
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-11 20:35:52 +0200

    refactor track pooling: DRY SQL logic, dynamic genres

--- server/src/data/tracks.js
+++ server/src/data/tracks.js
@@ -1,38 +1,44 @@
 import pool from "./pool.js";
 
-const getTracks = async ([rock, pop, rap]) => {
+const fetchTracksByGenres = async (
+  genres,
+  allowMultipleTracksPerArtist = true
+) => {
+  const placeholders = genres.map(() => "?").join(" OR g.name = ");
   const sql = `
-    SELECT t.id, t.deezer_id, t.title, t.duration, t.rank, t.preview,
-    a.deezer_id AS artist_id, a.name AS artist, g.name AS genre
+    SELECT 
+      t.id, 
+      t.deezer_id, 
+      t.title, 
+      t.duration, 
+      t.rank, 
+      t.preview,
+      a.deezer_id AS artist_id, 
+      a.name AS artist, 
+      g.name AS genre
     FROM tracks AS t
-    JOIN genres AS g
-    ON g.deezer_id = t.genre_deezer_id
-    JOIN artists AS a
-    ON a.deezer_id = t.artist_deezer_id
-    WHERE g.name = ? OR g.name = ? OR g.name = ?
-    ORDER BY rand()
+    JOIN genres AS g ON g.deezer_id = t.genre_deezer_id
+    JOIN artists AS a ON a.deezer_id = t.artist_deezer_id
+    WHERE g.name = ${placeholders}
+    ${allowMultipleTracksPerArtist ? "" : "GROUP BY a.deezer_id"}
+    ORDER BY RAND()
   `;
 
-  const result = await pool.query(sql, [rock, pop, rap]);
-  return result[0];
+  try {
+    const [result] = await pool.query(sql, genres);
+    return result;
+  } catch (error) {
+    console.error("Error executing query:", error);
+    throw error;
+  }
 };
 
-const getTracksNotRepArtists = async ([rock, pop, rap]) => {
-  const sql = `
-    SELECT t.id, t.deezer_id, t.title, t.duration, t.rank, t.preview,
-    a.deezer_id AS artist_id, a.name AS artist, g.name AS genre
-    FROM tracks AS t
-    JOIN genres AS g
-    ON g.deezer_id = t.genre_deezer_id
-    JOIN artists AS a
-    ON a.deezer_id = t.artist_deezer_id
-    WHERE g.name = ? OR g.name = ? OR g.name = ?
-    GROUP BY a.deezer_id
-    ORDER BY rand()
-  `;
+const getTracks = async (genres) => {
+  return await fetchTracksByGenres(genres, true);
+};
 
-  const result = await pool.query(sql, [rock, pop, rap]);
-  return result[0];
+const getTracksNotRepArtists = async (genres) => {
+  return await fetchTracksByGenres(genres, false);
 };
 
 export default {
