958da43f0f1c62d2b7a57c64aa67ff36f5136d95
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-13 19:57:22 +0200

    refactor playlist generator: simplify logic, improve readability

--- server/src/service/playlist-services.js
+++ server/src/service/playlist-services.js
@@ -3,89 +3,117 @@ import playlistData from "../data/playlists.js";
 import genresData from "../data/genres.js";
 
 export const playlistGenerator = async (req) => {
-  const duration = req.body.points.duration;
-  const playlistName = req.body.playlistName;
+  const { duration, playlistName, genres, repeatArtist } = req.body;
+  const userId = req.user.id;
 
-  const genresDuration = req.body.genres.map((g) =>
-    g.duration === 0
-      ? g
-      : { ...g, duration: Math.round((duration * g.duration) / 100) }
-  );
-  const genresName = Object.values(
-    req.body.genres
-      .map((g) =>
-        g.duration === 0 ? { ...g, duration: null } : { ...g, duration: g.name }
-      )
-      .reduce((acc, g) => {
-        const a = { [g.name]: g.duration };
-        return { ...acc, ...a };
-      }, {})
-  );
-
-  let tracksAll = [];
-
-  if (req.body.repeatArtist === false) {
-    const result = await tracksData.getTracksNotRepArtists(genresName);
-    tracksAll = result.filter((t) => t.hasOwnProperty("id"));
-  } else {
-    const result = await tracksData.getTracks(genresName);
-    tracksAll = result.filter((t) => t.hasOwnProperty("id"));
-  }
-
-  const generateTracksList = (tr, gen) => {
-    const result = gen.map((g) => {
-      let totalDuration = 0;
-      let a = [];
-      const temp = new Set();
-      let tracksFiltered = tr.filter((t) => t.genre.toLowerCase() === g.name);
-      while (totalDuration < g.duration) {
-        temp.add(tracksFiltered.pop());
-        a = [...temp];
-        totalDuration = a.reduce((acc, t) => (acc += t.duration), 0);
-        tracksFiltered = tracksFiltered.filter(
-          (t) => t.duration < g.duration * 1.05 - totalDuration
-        );
-      }
-      return temp;
-    });
-    return result;
+  const getGenresDuration = () => {
+    return genres.map((genre) =>
+      genre.duration === 0
+        ? genre
+        : { ...genre, duration: Math.round((duration * genre.duration) / 100) }
+    );
   };
 
-  const tracksList = generateTracksList(tracksAll, genresDuration).reduce(
-    (acc, arr) => [...acc, ...arr],
-    []
+  const getGenresName = () => {
+    return Object.values(
+      genres
+        .map((genre) =>
+          genre.duration === 0
+            ? { ...genre, duration: null }
+            : { ...genre, duration: genre.name }
+        )
+        .reduce((acc, genre) => {
+          acc[genre.name] = genre.duration;
+          return acc;
+        }, {})
+    );
+  };
+
+  const genresDuration = getGenresDuration();
+  const genresName = getGenresName();
+
+  const fetchTracks = async () => {
+    const fetchTracksFunc = repeatArtist
+      ? tracksData.getTracks
+      : tracksData.getTracksNotRepArtists;
+    const tracks = await fetchTracksFunc(genresName);
+    return tracks.filter((track) => track.hasOwnProperty("id"));
+  };
+
+  const tracksAll = await fetchTracks();
+
+  const generateTracksList = (tracks, genresDuration) => {
+    return genresDuration.map((genre) => {
+      const tracksFiltered = tracks.filter(
+        (track) => track.genre.toLowerCase() === genre.name
+      );
+      let totalDuration = 0;
+      let temp = new Set();
+      let tracksSubset = [];
+
+      while (totalDuration < genre.duration) {
+        const track = tracksFiltered.pop();
+        if (track) {
+          temp.add(track);
+          tracksSubset = [...temp];
+          totalDuration = tracksSubset.reduce((acc, t) => acc + t.duration, 0);
+          tracksFiltered = tracksFiltered.filter(
+            (track) => track.duration < genre.duration * 1.05 - totalDuration
+          );
+        }
+      }
+      return tracksSubset;
+    });
+  };
+
+  const tracksList = generateTracksList(tracksAll, genresDuration).flat();
+  const playlistDuration = tracksList.reduce(
+    (acc, track) => acc + track.duration,
+    0
   );
-  const playlistDuration = tracksList.reduce((acc, t) => acc + t.duration, 0);
   const averagePlaylistRank = Math.round(
-    tracksList.reduce((acc, t) => acc + t.rank, 0) / tracksList.length
+    tracksList.reduce((acc, track) => acc + track.rank, 0) / tracksList.length
   );
 
   const playlistDataObject = {
     name: playlistName,
     duration: playlistDuration,
-    user: req.user.id,
+    user: userId,
     rank: averagePlaylistRank,
   };
 
   const newPlaylist = await playlistData.setPlaylist(playlistDataObject);
 
-  await Promise.all(
-    tracksList.map(async (track) =>
-      playlistData.addTrackToPlaylist(newPlaylist.id, track.id, track.deezer_id)
-    )
-  );
-  await Promise.all(
-    req.body.genres.map(async (g) => {
-      if (g.duration !== 0) {
-        const genreId = await genresData.getGenreByName(g.name);
-        await playlistData.addPlaylistToGenre(
-          genreId.id,
-          genreId.deezer_id,
-          newPlaylist.id
-        );
-      }
-    })
-  );
+  const addTracksToPlaylist = async () => {
+    await Promise.all(
+      tracksList.map((track) =>
+        playlistData.addTrackToPlaylist(
+          newPlaylist.id,
+          track.id,
+          track.deezer_id
+        )
+      )
+    );
+  };
+
+  await addTracksToPlaylist();
+
+  const addGenresToPlaylist = async () => {
+    await Promise.all(
+      genres.map(async (genre) => {
+        if (genre.duration !== 0) {
+          const genreId = await genresData.getGenreByName(genre.name);
+          await playlistData.addPlaylistToGenre(
+            genreId.id,
+            genreId.deezer_id,
+            newPlaylist.id
+          );
+        }
+      })
+    );
+  };
+
+  await addGenresToPlaylist();
 
   return newPlaylist;
 };
