db1b33fa44647dd6209b18a78c08b219b1c6a995
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-14 13:14:43 +0200

    move travel points state to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./providers/auth-context";
 import NavBar from "./components/Navbar";
@@ -6,19 +6,13 @@ import Routes from "./Routes";
 import "./scss/style.scss";
 
 const App = () => {
-  const [points, setPoints] = useState({
-    duration: 0,
-    from: "",
-    to: "",
-  });
-
   return (
     <div className="app-container">
       <BrowserRouter>
         <AuthContextProvider>
           <NavBar />
           <main>
-            <Routes points={points} setPoints={setPoints} />
+            <Routes />
           </main>
         </AuthContextProvider>
       </BrowserRouter>
--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import GuardedRoute from "./providers/GuardedRoute";
 import Home from "./pages/Home";
@@ -10,27 +11,39 @@ import GeneratePlaylist from "./pages/GeneratePlaylist";
 import ForgottenPassword from "./pages/ForgottenPassword";
 import PageNotFound from "./pages/PageNotFound";
 
-const Routes = ({ points, setPoints }) => (
-  <Switch>
-    <Redirect path="/" exact to="/home" />
-    <Route exact path="/home" component={Home} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/register" component={Register} />
-    <Route exact path="/forgotten-password" component={ForgottenPassword} />
-    <GuardedRoute exact admin path="/users" component={Users} />
-    <GuardedRoute
-      exact
-      path="/generate-route"
-      component={(props) => <GenerateRoute {...props} setPoints={setPoints} />}
-    />
-    <GuardedRoute
-      exact
-      path="/generate-playlist"
-      component={(props) => <GeneratePlaylist {...props} points={points} />}
-    />
-    <Route exact path="/playlists/:id" component={SinglePlaylist} />
-    <Route path="*" component={PageNotFound} />
-  </Switch>
-);
+const Routes = () => {
+  const [travelPoints, setTravelPoints] = useState({
+    duration: 0,
+    from: "",
+    to: "",
+  });
+
+  return (
+    <Switch>
+      <Redirect path="/" exact to="/home" />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/forgotten-password" component={ForgottenPassword} />
+      <GuardedRoute exact admin path="/users" component={Users} />
+      <GuardedRoute
+        exact
+        path="/generate-route"
+        component={(props) => (
+          <GenerateRoute {...props} setPoints={setTravelPoints} />
+        )}
+      />
+      <GuardedRoute
+        exact
+        path="/generate-playlist"
+        component={(props) => (
+          <GeneratePlaylist {...props} points={travelPoints} />
+        )}
+      />
+      <Route exact path="/playlists/:id" component={SinglePlaylist} />
+      <Route path="*" component={PageNotFound} />
+    </Switch>
+  );
+};
 
 export default Routes;
