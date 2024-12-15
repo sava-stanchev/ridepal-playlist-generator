9eae34eb7b68cb009f0f689e97e52370bf798bb4
Sava Stanchev <s.stanchev94@gmail.com>
2024-12-15 21:17:50 +0200

    change favicon, remove portal, refactor index js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./store/reducers";
+import App from "./App";
 
 const store = configureStore({ reducer: rootReducer });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
